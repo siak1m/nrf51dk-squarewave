04b92d7fac61542c228c05fa89de9c90de7df0b4
siak1m <siak1m@naver.com>
2024-02-25 16:25:58 +0900

    back up for adc3port+i2c+ble

--- pca10028/s130/arm5_no_packs/_build/main.d
+++ pca10028/s130/arm5_no_packs/_build/main.d
@@ -69,3 +69,8 @@
 .\_build\main.o: ..\..\..\..\..\..\components\libraries\util\sdk_common.h
 .\_build\main.o: ..\..\..\..\..\..\components\libraries\util\sdk_os.h
 .\_build\main.o: ..\..\..\..\..\..\components\libraries\util\sdk_macros.h
+.\_build\main.o: ..\..\..\..\..\..\components\drivers_nrf\common\nrf_drv_common.h
+.\_build\main.o: ..\..\..\..\..\..\components\libraries\log\nrf_log.h
+.\_build\main.o: ..\..\..\..\..\..\components\libraries\log\src\nrf_log_internal.h
+.\_build\main.o: ..\..\..\..\..\..\components\libraries\log\nrf_log_ctrl.h
+.\_build\main.o: ..\..\..\..\..\..\components\libraries\log\src\nrf_log_ctrl_internal.h
